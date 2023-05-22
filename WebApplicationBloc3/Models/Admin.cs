ca8e5496ed9023223dae1026a6f9b1a4039cad26
Anna <hertrich.anna@gmail.com>
2023-05-22 21:06:45 +0200

    Yo yo

---
+++ WebApplicationBloc3/Models/Admin.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationBloc3.Models
+{
+    public class Admin
+    {
+        public int Id_admin { get; set; }
+        public string Name { get; set; }
+      
+    }
+}
